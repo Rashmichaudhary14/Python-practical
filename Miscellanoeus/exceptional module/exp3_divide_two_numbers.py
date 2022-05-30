6d2edbd85813c5709741b988cf437f85a8d1f912
Rashmichaudhary14 <106426179+Rashmichaudhary14@users.noreply.github.com>
2022-05-30 20:40:51 +0530

    Divide_two_numbers.

---
+++ Miscellanoeus/exceptional module/exp3_divide_two_numbers.py
@@ -0,0 +1,7 @@
+#divide two numbers
+def divide_two_numbers():
+    a = input("Enter an integer: ")    
+    b = input("Enter another integer: ")
+    c = int(a)/int(b)
+    print(c)
+divide_two_numbers()
